ca6660f57509052f15ed58c5693fcd88343baaff
Alok Kumar <kumaralok444@gmail.com>
2016-06-10 11:07:21 +0530

    First Class
    A sample WebDriver code

--- src/basicsClass1/Test1.java
+++ src/basicsClass1/Test1.java
@@ -1,5 +1,7 @@
 package basicsClass1;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,15 +10,17 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 public class Test1 {
 	public static void main(String args[])
 	{
-		//FirefoxProfile profile = new FirefoxProfile();
-		//FirefoxBinary binary = new FirefoxBinary();
-		//WebDriver driver = new FirefoxDriver(binary,profile);     
-		//WebDriver driver=new FirefoxDriver();
-		System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
-	    FirefoxDriver driver = new FirefoxDriver();
+		File pathBinary = new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+		FirefoxBinary Binary = new FirefoxBinary(pathBinary);
+		FirefoxProfile firefoxPro = new FirefoxProfile();       
+		WebDriver driver = new FirefoxDriver(Binary,firefoxPro);
 		driver.get("https://www.google.co.in/");
 		String title=driver.getTitle();
 		System.out.println("Title of page is "+title);
+		String url=driver.getCurrentUrl();
+		System.out.println("URL of page is "+url);
+		String wid=driver.getWindowHandle();
+		System.out.println("Id of Window is "+wid);
 	}
 
 }
